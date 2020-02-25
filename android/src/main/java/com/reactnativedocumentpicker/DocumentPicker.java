997c1b1dd32952a9f10bb3bae7f6c3f09540e8b6
obaydah <obayda7h@gmail.com>
2020-02-25 17:01:07 +0200

    Doc to base64 support on Android

--- android/src/main/java/com/reactnativedocumentpicker/DocumentPicker.java
+++ android/src/main/java/com/reactnativedocumentpicker/DocumentPicker.java
@@ -9,6 +9,8 @@ import android.os.Bundle;
 import android.provider.OpenableColumns;
 import android.util.Log;
 import android.webkit.MimeTypeMap;
+import android.util.Base64;
+import android.content.Context;
 
 import com.facebook.react.bridge.ActivityEventListener;
 import com.facebook.react.bridge.Arguments;
@@ -26,6 +28,11 @@ import java.io.IOException;
 import java.net.URL;
 import java.nio.channels.Channels;
 import java.nio.channels.ReadableByteChannel;
+import java.io.ByteArrayOutputStream;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
 /**
  * @see <a href="https://developer.android.com/guide/topics/providers/document-provider.html">android documentation</a>
@@ -38,14 +45,18 @@ public class DocumentPicker extends ReactContextBaseJavaModule implements Activi
         private static final String FILE_SIZE = "fileSize";
         private static final String FILE_NAME = "fileName";
         private static final String TYPE = "type";
+        private static final String DATA = "data";
     }
 
     private Callback callback;
 
     public DocumentPicker(ReactApplicationContext reactContext) {
         super(reactContext);
-        reactContext.addActivityEventListener(this);
+        this.reactContext = reactContext;
+        this.reactContext.addActivityEventListener(this);
     }
+
+    private final ReactApplicationContext reactContext;
 
     @Override
     public String getName() {
@@ -78,6 +89,47 @@ public class DocumentPicker extends ReactContextBaseJavaModule implements Activi
     public void onActivityResult(Activity activity, int requestCode, int resultCode, Intent data) {
         onActivityResult(requestCode, resultCode, data);
     }
+
+    private File createFileFromURI(Uri uri) throws Exception {
+        File file = new File(reactContext.getExternalCacheDir(), "document-" + uri.getLastPathSegment());
+        InputStream input = reactContext.getContentResolver().openInputStream(uri);
+        OutputStream output = new FileOutputStream(file);
+        try {
+            byte[] buffer = new byte[4 * 1024];
+            int read;
+            while ((read = input.read(buffer)) != -1) {
+                output.write(buffer, 0, read);
+            }
+            output.flush();
+        } finally {
+            output.close();
+            input.close();
+        }
+        return file;
+    }
+
+    private String getBase64StringFromFile(String absoluteFilePath) {
+        InputStream inputStream = null;
+        try {
+            inputStream = new FileInputStream(new File(absoluteFilePath));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        byte[] bytes;
+        byte[] buffer = new byte[8192];
+        int bytesRead;
+        ByteArrayOutputStream output = new ByteArrayOutputStream();
+        try {
+            while ((bytesRead = inputStream.read(buffer)) != -1) {
+                output.write(buffer, 0, bytesRead);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        bytes = output.toByteArray();
+        return Base64.encodeToString(bytes, Base64.NO_WRAP);
+  }
 
     // removed @Override temporarily just to get it working on RN0.33 and RN0.32
     public void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -119,17 +171,26 @@ public class DocumentPicker extends ReactContextBaseJavaModule implements Activi
     }
 
     private WritableMap metaDataFromUri(Uri uri) {
-        WritableMap map = Arguments.createMap();
+        WritableMap map = Arguments.createMap(); 
 
         File outputDir = getReactApplicationContext().getCacheDir();
-        try {
-            File downloaded = download(uri, outputDir);
 
-            map.putDouble(Fields.FILE_SIZE, new Long(downloaded.length()).doubleValue());
-            map.putString(Fields.FILE_NAME, downloaded.getName());
-            map.putString(Fields.TYPE, mimeTypeFromName(uri.toString()));
-        } catch (IOException e) {
-            Log.e("DocumentPicker", "Failed to download file", e);
+        try{
+            File file = createFileFromURI(uri);
+            try {
+                File downloaded = download(uri, outputDir);
+
+                map.putDouble(Fields.FILE_SIZE, new Long(downloaded.length()).doubleValue());
+                map.putString(Fields.FILE_NAME, downloaded.getName());
+                map.putString(Fields.TYPE, mimeTypeFromName(uri.toString()));
+                map.putString(Fields.DATA, getBase64StringFromFile(file.getAbsolutePath()));
+            } catch (IOException e) {
+                Log.e("DocumentPicker", "Failed to download file", e);
+            }
+        }
+        catch (Exception e){
+            // document not in cache
+            Log.e("ERROR", "Error Message: ", e);
         }
 
         return map;
@@ -144,6 +205,7 @@ public class DocumentPicker extends ReactContextBaseJavaModule implements Activi
         map.putDouble(Fields.FILE_SIZE, new Long(file.length()).doubleValue());
         map.putString(Fields.FILE_NAME, file.getName());
         map.putString(Fields.TYPE, mimeTypeFromName(file.getAbsolutePath()));
+        map.putString(Fields.DATA, getBase64StringFromFile(file.getAbsolutePath()));
 
         return map;
     }
@@ -168,6 +230,13 @@ public class DocumentPicker extends ReactContextBaseJavaModule implements Activi
                     if (size != null)
                         map.putDouble(Fields.FILE_SIZE, Double.valueOf(size));
                 }
+                try{
+                    File file = createFileFromURI(uri);
+                    map.putString("data", getBase64StringFromFile(file.getAbsolutePath()));
+                }
+                catch (Exception e){
+                    Log.e("ERROR", "Error Message: ", e);
+                }
             }
         } finally {
             if (cursor != null) {
@@ -176,6 +245,10 @@ public class DocumentPicker extends ReactContextBaseJavaModule implements Activi
         }
 
         return map;
+    }
+
+    public Context getContext(){
+        return getReactApplicationContext();
     }
 
     private static File download(Uri uri, File outputDir) throws IOException {
